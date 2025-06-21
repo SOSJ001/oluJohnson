6a793f2fc88971e4f02a9b0078d6ec320c8b7d13
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2025-06-21 06:50:20 +0100

    company image updated fix
    fix of Trusted company logo

--- src/lib/components/TrustedCompanies.svelte
+++ src/lib/components/TrustedCompanies.svelte
@@ -1,17 +1,23 @@
 <script>
-	let companies = ["/Bloomlogo.png", "/gtbank.png", "/zenith.png", "/limkokwingUni.png", "/vitaFoam.png"];
+	let companies = [
+		'/Bloomlogo.png',
+		'/gtbank.png',
+		'/zenith.png',
+		'/limkokwingUni.png',
+		'/vitaFoam.png'
+	];
 </script>
 
 <section class="py-10">
 	<div class="container mx-auto px-4">
 		<h2 class="mb-8 text-2xl font-bold">Trusted By</h2>
-		<div class="flex items-center justify-center space-x-10 overflow-x-auto pb-4">
+		<div class="flex md:items-center md:justify-center space-x-10 overflow-x-auto pb-4">
 			{#each companies as i}
 				<img
-                loading="lazy"
+					loading="lazy"
 					src={i}
 					alt={`Logo ${i}`}
-					class="h-32 w-32 flex-shrink-0 rounded object-contain shadow-sm"
+					class="h-32 flex-shrink-0 rounded object-contain shadow-sm w-32"
 				/>
 			{/each}
 		</div>
