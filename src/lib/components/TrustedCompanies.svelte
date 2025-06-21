1930011e5ee15776e5590cbc3dde3dc7916e6134
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2025-06-21 08:00:05 +0100

    trusted Company updated

--- src/lib/components/TrustedCompanies.svelte
+++ src/lib/components/TrustedCompanies.svelte
@@ -1,10 +1,12 @@
 <script>
-	import { AirplayIcon, CastIcon } from "svelte-feather-icons";
+	import { AirplayIcon, CastIcon } from 'svelte-feather-icons';
 
 	let companies = [
 		'/Bloomlogo.png',
 		'/gtbank.png',
 		'/zenith.png',
+		'/uba.png',
+		'vista.png',
 		'/limkokwingUni.png',
 		'/vitaFoam.png'
 	];
@@ -13,13 +15,13 @@
 <section class="py-10">
 	<div class="container mx-auto px-4">
 		<h2 class="mb-8 text-2xl font-bold">Trusted By</h2>
-		<div class="flex md:items-center md:justify-center space-x-10 overflow-x-auto pb-4">
+		<div class="flex space-x-10 overflow-x-auto pb-4 md:items-center md:justify-center">
 			{#each companies as i}
 				<img
 					loading="lazy"
 					src={i}
 					alt={`Logo ${i}`}
-					class="h-32 flex-shrink-0 rounded object-contain shadow-sm w-32"
+					class="h-32 w-32 flex-shrink-0 rounded object-contain shadow-sm"
 				/>
 			{/each}
 		</div>
